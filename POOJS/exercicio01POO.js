ca51ca74d1072d0dc3cbdca113f67972b2bb657b
gabriel.hds.senai@gmail.com <gabriel.hds.senai@gmail.com>
2025-04-29 16:54:31 -0300

    29.04.25

---
+++ POOJS/exercicio01POO.js
@@ -0,0 +1,31 @@
+// Atividade 1: Criar uma classe representando um produto
+
+class Produto {
+    constructor(nome, preco, estoque){
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    vender(qtd){
+        if(this.estoque<qtd){
+            console.log("Estoque insificiente!");
+            return;
+        }
+        return this.estoque -= qtd;
+    }
+    repor(qtd){
+        return this.estoque += qtd;
+    }
+
+    exibirInfo(){
+        console.log(`Produto: ${this.nome}, Preco: ${this.preco}, Estoque: ${this.estoque}`);
+    }
+}
+
+let produto1 = new Produto("Arroz", 30, 100);
+produto1.exibirInfo();
+produto1.vender(110);
+produto1.repor(20);
+produto1.vender(110);
+produto1.exibirInfo();
